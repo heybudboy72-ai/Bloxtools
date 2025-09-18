07152f50c8b9752c0f51896dc6cc3d8e66819d45
heybudboy72-ai <heybudboy72@gmail.com>
2025-09-17 22:34:38 -0400

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,8 +1,9 @@
 <?php
-# ItsYaboyNathan
-$name = "Nathan";
+# Enter Price
+$name = "Enter";
 $discord = "INPUT UR DISCORD SERVER HERE";
 $image = "https://i.pinimg.com/originals/e9/3c/76/e93c766198a8db2656d42fc17dca722f.gif";
 $color = "00000";
 $adminhook = "";
+
 ?>
